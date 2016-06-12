12bb8455c1c19c1c35b3e042166fd665994e2191
Andrew Lawrence <andrewlawrence.05@gmail.com>
2016-06-11 23:02:31 -0400

    redid onboarding

---
+++ purch/LogInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  purch
+//
+//  Created by Andrew Lawrence on 2016-06-11.
+//  Copyright © 2016 Andrew Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
